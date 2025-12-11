5f45c74775c5081581693f3c1b1f9a4d88dadcac
Ekpe Prince <ekpep666@gmail.com>
2025-12-11 22:21:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "rentfast-v2",
+  "appId": "1:685830534650:web:7ac3049c1c71536072e043",
+  "apiKey": "AIzaSyAol1fmKfMZ9M0AyYw9LBfJNkPcTOWKY5Q",
+  "authDomain": "rentfast-v2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "685830534650"
+};
