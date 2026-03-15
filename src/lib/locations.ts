39c7727dd08d1fe48e29065a2e20c754940f2d4b
Ekpe Prince <ekpep666@gmail.com>
2026-03-15 17:38:02 +0000

    we starting with the south south state in nigeria for now

--- src/lib/locations.ts
+++ src/lib/locations.ts
@@ -1,29 +1,46 @@
 /**
- * Standardized list of major Nigerian property hubs.
+ * Standardized list of major South South Nigerian property hubs.
  * Used for autocomplete in search and listing creation.
  */
 export const NIGERIAN_LOCATIONS = [
-  "Lekki Phase 1, Lagos",
-  "Ikeja GRA, Lagos",
-  "Victoria Island, Lagos",
-  "Ikoyi, Lagos",
-  "Yaba, Lagos",
-  "Surulere, Lagos",
-  "Ajah, Lagos",
-  "Gbagada, Lagos",
-  "Magodo, Lagos",
-  "Maitama, Abuja",
-  "Asokoro, Abuja",
-  "Wuse, Abuja",
-  "Garki, Abuja",
-  "Jabi, Abuja",
-  "Gwarinpa, Abuja",
+  // Rivers State
   "GRA Phase 1, Port Harcourt",
   "GRA Phase 2, Port Harcourt",
+  "GRA Phase 3, Port Harcourt",
   "Trans Amadi, Port Harcourt",
-  "Bodija, Ibadan",
-  "Enugu",
-  "Benin City",
-  "Kano",
-  "Kaduna"
+  "Woji, Port Harcourt",
+  "Peter Odili Road, Port Harcourt",
+  "Ada George, Port Harcourt",
+  "Elelenwo, Port Harcourt",
+  "D-Line, Port Harcourt",
+  "Iwofe, Port Harcourt",
+  // Akwa Ibom State
+  "Ewet Housing Estate, Uyo",
+  "Shelter Afrique, Uyo",
+  "Osongama Estate, Uyo",
+  "Abak Road, Uyo",
+  "Oron Road, Uyo",
+  // Delta State
+  "GRA, Asaba",
+  "Okpanam Road, Asaba",
+  "DBS Road, Asaba",
+  "Effurun, Warri",
+  "DSC Township, Warri",
+  "Refinery Road, Warri",
+  // Edo State
+  "GRA, Benin City",
+  "Airport Road, Benin City",
+  "Sapele Road, Benin City",
+  "Ugbowo, Benin City",
+  "Ekenwan Road, Benin City",
+  // Cross River State
+  "State Housing Estate, Calabar",
+  "Federal Housing Estate, Calabar",
+  "MCC Road, Calabar",
+  "Etta Agbo, Calabar",
+  // Bayelsa State
+  "Amarata, Yenagoa",
+  "Ekeki, Yenagoa",
+  "Biogbolo, Yenagoa",
+  "Azikoro, Yenagoa"
 ];
